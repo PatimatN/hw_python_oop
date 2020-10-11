e8181853353f706c73d0161b00124850a45e61b9
Patimat <patimat.n.m@yandex.ru>
2020-10-11 15:22:09 +0300

    removed extra code

--- homework.py
+++ homework.py
@@ -79,13 +79,3 @@ class CaloriesCalculator(Calculator):
                     f" калорийностью не более {leftover} кКал")
         else:
             return "Хватит есть!"
-
-
-cash_calculator = CashCalculator(1000)
-cash_calculator.add_record(Record(amount=145, comment="кофе")) 
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-                
-print(cash_calculator.get_today_cash_remained("rub"))
